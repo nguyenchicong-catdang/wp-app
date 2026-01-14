c0ecd1bc3e013b77b62ee9fadafd7465dc25a258
nguyenchicong <nguyenchicong.catdang@gmail.com>
2026-01-14 09:32:26 +0700

    up default setup

---
+++ adminer/run.md
@@ -0,0 +1,6 @@
+# run
+
+> sudo service mysql start
+
+> cd ~/git/wp-app/adminer && php -S 127.0.0.1:9000
+
--- config/filesystems.php
+++ config/filesystems.php
@@ -75,6 +75,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('uploads') => __DIR__. "/../wordpress/uploads",
     ],
 
 ];
